6c1097a054e7c0e1a517b0df62135aa3e4b131c0
ACEphoenix193 <shouvikm30@gmail.com>
2022-09-09 23:10:24 +0400

    final update

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ CORS(app)
 def index_get():
     return render_template("base.html")
 
+@app.get("/team")
+def team_get():
+    return render_template("team.html")
+
 @app.post("/predict")
 def predict():
     text = request.get_json().get("message")
